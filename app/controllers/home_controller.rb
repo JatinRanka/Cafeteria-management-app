670cc29835cd058ffcd46d43030276153fabc4ca
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-26 14:05:10 +0530

    Home page displays menu & cart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
     @menu_items = MenuItem.all()
+    @cart_items = []
+
+    cart = Cart.find_by(user_id: @current_user.id)
+    if cart
+      @cart_items = cart.cart_items
+    end
+
     render "index"
   end
 end
