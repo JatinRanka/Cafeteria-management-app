79b73d9ea8d3e85f0bc7f3c7794ba98d7cfd42fc
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-26 14:45:07 +0530

    Added function to check if item is present in cart

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,15 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu_categorie
+
+  def is_present_in_cart?(current_user)
+    user_cart = Cart.find_by(user_id: current_user.id)
+
+    if user_cart
+      if CartItem.find_by(cart_id: user_cart.id, menu_item_id: id)
+        return true
+      end
+    end
+
+    return false
+  end
 end
