fd8b3078d6a4797235fa702cee91830fcd77e276
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-26 15:52:57 +0530

    Added menu qty increment decrement functionality

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -20,6 +20,7 @@ class CartItemsController < ApplicationController
   def create
     cart = Cart.find_by(user_id: @current_user.id)
 
+    # Creates cart for user if it doesn't exist
     unless cart
       cart = Cart.create!(
         user_id: @current_user.id
@@ -33,8 +34,28 @@ class CartItemsController < ApplicationController
       menu_item_id: menu_item_id,
       menu_item_name: menu_item.name,
       menu_item_price: menu_item.price,
+      quantity: 1,
       cart_id: cart.id
     )
+
+    redirect_to home_path
+
+  end
+
+  def update
+    cart_item_id = params[:id]
+    update_type = params[:type]
+
+    if update_type == "increment"
+      CartItem.find(cart_item_id).increment!(:quantity)
+    else # decrement
+      cart_item = CartItem.find(cart_item_id).decrement!(:quantity)
+
+      if cart_item.quantity == 0
+        cart_item.delete()
+      end
+
+    end
 
     redirect_to home_path
 
