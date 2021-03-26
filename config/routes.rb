0a3334a49107156fe2deb1c4449760a2c1c40e5e
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-26 14:06:02 +0530

    Updated cart model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/" => "home#index"
+  get "/" => "home#index", as: :home
 
   resources :users
   resources :menu_items
   resources :menu_categories
+  resources :cart_items
+
+  get "/cart" => "cart#index", as: :cart
+  post "/cart" => "cart#create", as: :cart_new
 
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
