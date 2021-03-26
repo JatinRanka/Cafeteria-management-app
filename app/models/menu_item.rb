57fbd5713305209ba31477d2fb21f13c052d60c2
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-26 15:53:29 +0530

    Added function to fetch cart item

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,12 +1,14 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu_categorie
 
-  def is_present_in_cart?(current_user)
+  #Return the corresponding cart_item for a particular menu_item_id
+  def fetch_cart_item(current_user)
     user_cart = Cart.find_by(user_id: current_user.id)
 
     if user_cart
-      if CartItem.find_by(cart_id: user_cart.id, menu_item_id: id)
-        return true
+      cart_item = CartItem.find_by(cart_id: user_cart.id, menu_item_id: id)
+      if cart_item
+        return cart_item
       end
     end
 
