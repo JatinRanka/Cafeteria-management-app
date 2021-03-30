a2d4523e0f2e727d7e712b6398e15b9e06449e2f
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-30 12:16:23 +0530

    Added functionality to update menu items

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -17,4 +17,29 @@ class MenuItemsController < ApplicationController
 
     redirect_to menu_items_path
   end
+
+  def edit
+    id = params[:id]
+    @menu_item = MenuItem.find(id)
+    @menu_categories = MenuCategorie.all
+    render "edit"
+  end
+
+  def update
+    id = params[:id]
+    name = params[:name]
+    price = params[:price]
+    description = params[:description]
+    menu_categorie_id = params[:menu_categorie_id]
+
+    menu_item = MenuItem.find(id)
+    menu_item.name = name
+    menu_item.price = price
+    menu_item.description = description
+    menu_item.menu_categorie_id = menu_categorie_id
+    menu_item.save
+
+    flash[:success] = "Menu item updated."
+    redirect_to menu_categories_path
+  end
 end
