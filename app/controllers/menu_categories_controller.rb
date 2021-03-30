441aad102e16d8456be24207c86a7f2513f9f1b0
jatin-ranka <jatin.ranka@freshworks.com>
2021-03-30 12:15:48 +0530

    Added functionality to update menu categories

--- app/controllers/menu_categories_controller.rb
+++ app/controllers/menu_categories_controller.rb
@@ -14,4 +14,23 @@ class MenuCategoriesController < ApplicationController
     redirect_to menu_categories_path
   end
 
+  def edit
+    id = params[:id]
+    @menu_categorie = MenuCategorie.find(id)
+    render "edit"
+  end
+
+  def update
+    id = params[:id]
+    name = params[:name]
+
+    menu_categorie = MenuCategorie.find(id)
+    menu_categorie.name = name
+    menu_categorie.save
+
+    flash[:success] = "Menu category updated."
+    redirect_to menu_categories_path
+
+  end
+
 end
